d0958eac33c52465605a32f9baebe69117df2a26
magal2003 <ivan.almarazg200@gmail.com>
2025-09-24 12:08:11 -0600

    chore: initial commit with Spring Boot project structure

---
+++ src/main/java/com/example/gestordetareas/GestorDeTareasApplication.java
@@ -0,0 +1,13 @@
+package com.example.gestordetareas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestorDeTareasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestorDeTareasApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gestordetareas/GestorDeTareasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gestordetareas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestorDeTareasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
